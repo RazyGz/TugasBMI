0d850b1d33904f81a45635de1b004ee8a85a89e7
RazyGz <aaron_cahya_32rpl@student.smktelkom-mlg.sch.id>
2023-08-10 09:05:37 +0700

    bmi

---
+++ Bmiberatbadan.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Bmiberatbadan {
+    public static void main(String[] args) {
+        //1. Membuat program sederhana menghitung BMI atau indeks masa tubuh.
+        Scanner input = new Scanner(System.in);
+
+        System .out.print("Masukkan Berat Badan : ");
+        double Berat = input.nextDouble();
+
+        System .out.print("Masukkan Berat Badan : ");
+        double Tinggi = input.nextDouble();
+        System .out.print();
+    }
+}
